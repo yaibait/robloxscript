dad45d6b71d7c11e947184dd5db9a2e79c6ebabd
yaibait <binhnguyen7968@gmail.com>
2024-11-20 00:11:42 +0700

    Create maru_kaitun.lua

---
+++ maru_kaitun.lua
@@ -0,0 +1,4 @@
+getgenv().Key = "MARU-XRKH0-534CH-FDOV-ZUP2O-Y28G"
+getgenv().id = "274545748174176256"
+getgenv().Script_Mode = "Kaitun_Script"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
